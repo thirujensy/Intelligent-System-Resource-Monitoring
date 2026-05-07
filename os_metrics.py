ba3ecbd1c6d93fd1f51dfa4c3dc2bac82423d6d4
THIRUMALAI D <thirudillimuthu@gmail.com>
2026-05-07 19:00:52 +0530

    Create os_metrics.py

---
+++ os_metrics.py
@@ -0,0 +1,9 @@
+import psutil
+
+
+def collect_metrics():
+    return {
+        "cpu": psutil.cpu_percent(interval=1),
+        "memory": psutil.virtual_memory().percent,
+        "disk": psutil.disk_usage('/').percent
+    }
